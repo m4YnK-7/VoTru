b569f8b62d1973e91ff61629d403c52c407d796f
nishantak09 <nishantak09@gmail.com>
2024-03-09 20:41:50 +0530

    dsbefn

--- contracts/Polls.sol
+++ contracts/Polls.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.0 <0.9.0;
+pragma solidity ^0.8.0;
 
-contract Election {
+contract Polls {
     enum State {
         NotStarted,
         InProgress,
