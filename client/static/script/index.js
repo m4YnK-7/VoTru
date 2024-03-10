d94b4d681dcd5d34b8ecb05167ba37e0f2ea3085
nishantak09 <nishantak09@gmail.com>
2024-03-10 09:36:41 +0530

    dir str

--- client/static/script/index.js
+++ client/static/script/index.js
@@ -1,4 +1,14 @@
-function animateDiv() {
-    var div = document.getElementById("layers");
-    div.style.transform = "translateX(-100dvw)"; 
+
+function changeContent() {
+    var animatedDiv = document.getElementById("more");
+    var replacementDiv = document.getElementById("register");
+
+    animatedDiv.style.transform = "translateX(-100%)";
+    replacementDiv.style.opacity = 1;
+}
+
+function admin(){
+    var content = document.getElementById("admin_register");
+    var second_div = document.getElementById("voter_register");
+    second_div.style.opacity = 0;
 }
