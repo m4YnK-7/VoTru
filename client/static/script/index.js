599f71a0308522ccfa93021ec9e4347942ea4eb1
Mayank Singh <mayanksingh4370@gmail.com>
2024-03-10 12:25:53 +0530

    Design changes

--- client/static/script/index.js
+++ client/static/script/index.js
@@ -9,6 +9,11 @@ function changeContent() {
 
 function admin(){
     var content = document.getElementById("admin_register");
-    var second_div = document.getElementById("voter_register");
-    second_div.style.opacity = 0;
+    // var second_div = document.getElementById("voter_register");
+    // second_div.style.opacity = 0;
+    var body = document.querySelector('body');
+    // body.style.background = "linear-gradient(135deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0))";
+    body.style.backdropFilter = "blur(10px)";
+    // body.style.w
+    // -webkit-backdrop-filter: blur(10px);
 }
